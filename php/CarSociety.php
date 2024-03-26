b0966d7fa135deeaa81b3822fbf976a5b9024f60
alexandregrisez <104682287+alexandregrisez@users.noreply.github.com>
2024-03-26 08:34:12 +0100

    Update CarSociety.php

--- php/CarSociety.php
+++ php/CarSociety.php
@@ -1,8 +1,6 @@
 <?php
     // SCRIPT POUR (RE)INITIALISER LA BASE DE DONNEES DE CAR SOCIETY
-    // (COMPTE ROOT SANS MOT DE PASSE)
-    session_start();
-
+    // (COMPTE ROOT SANS MOT DE PASSE
     function carsociety() {
         try {
             // Login utilisateur (compte root sans mot de passe)
