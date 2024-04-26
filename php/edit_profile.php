4db6741a07f68aebdc810751e36f6ab2c4095431
Quentin55553 <67703561+Quentin55553@users.noreply.github.com>
2024-04-26 02:49:45 +0200

    Add files via upload

--- php/edit_profile.php
+++ php/edit_profile.php
@@ -112,7 +112,7 @@
                         file_put_contents($usersFile, json_encode($users, JSON_PRETTY_PRINT));
 
                         // Met à jour le mot de passe dans la bdd sql
-                        require_once 'CarSocietyData.php';
+                        require_once '../bdd/CarSocietyData.php';
                         json_to_sql_users();
 
 
